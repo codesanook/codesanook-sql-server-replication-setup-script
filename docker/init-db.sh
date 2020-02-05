76f156e95455603c9055b8dcee3e842dc1861b66
aaronamm <theeranitp@gmail.com>
2020-02-05 08:17:04 +0700

    Set hostname and container name to all SQL Server instances

--- docker/init-db.sh
+++ docker/init-db.sh
@@ -3,5 +3,5 @@ echo "**** init db"
 echo  $SA_PASSWORD
 
 # Wait for the SQL Server to come up
-sleep 10s
+sleep 20s
 /opt/mssql-tools/bin/sqlcmd -U sa -P $SA_PASSWORD -i create-db.sql -d master &>/dev/null
